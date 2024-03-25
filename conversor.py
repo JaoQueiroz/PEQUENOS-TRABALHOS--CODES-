37a9630853e335ccb4fe0f44814f8d4a1798ce5c
João Queiroz <154699209+JaoQueiroz@users.noreply.github.com>
2024-03-25 01:36:04 -0300

    Add files via upload
    Contatos atualizados

--- conversor.py
+++ conversor.py
@@ -1,16 +1,16 @@
 #LER 2 VALORES E COM MENU DE OPERAÇÕES----------------------------EX 59
 from time import sleep
 print('ESTE MÓDULO CONVERTE O VALOR QUE VOCÊ TEM NA CARTEIRA EM REAIS (R$) PARA UM VALOR EM DÓLARES')
-print('-=-=-' * 21)
+line = str('-=-=-' * 21)
 sleep(1)
 print('QUANTO EM DINHEIRO R$ (REAIS) VOCÊ TEM NA CARTEIRA? ')
 n1 = float(input('POR FAVOR, USE PONTO (.) COMO SEPARADOR DOS CENTAVOS, CASO PRECISE - R$ '))
 n1_limitado = round(n1, 2)
-print('-=-=-' * 21)
+print(line)
 sleep(1)
 print('EM RELAÇÃO Á NOSSA MOEDA R$ (REAL), QUANTO ESTÁ VALENDO O DÓLAR (US$) NESTE MOMENTO? ')
 print('(FAÇA UMA BREVE PESQUISA CASO PRECISE) ')
-print('-=-=-' * 21)
+print(line)
 sleep(1)
 print('COTAÇÃO DÓLAR (US$) ATUAL ')
 n2 = float(input('POR FAVOR, USE PONTO (.) COMO SEPARADOR DOS CENTAVOS, CASO PRECISE - R$ '))
@@ -23,8 +23,14 @@ while opção != 3:
 	opção = int(input('>>>>>>QUAL É A SUA OPÇÃO? '))
 	if opção == 1:
 		produto = n1 / n2
+		print(line)
+		sleep(0.5)
+		print(line)
+		sleep(0.5)
 		print('TENDO R${} NA CARTEIRA, COM O DÓLAR VALENDO R${} REAIS, VOCÊ TERIA O EQUIVALENTE A US$ {:.2f} DÓLARES'.format(n1_limitado, n2_limitado, produto))
 	elif opção == 2:
+		print(line)
+		sleep(0.5)
 		print('INFORME OS NÚMEROS NOVAMENTE:')
 		print('QUANTO EM DINHEIRO R$ (REAIS) VOCÊ TEM NA CARTEIRA? ')
 		n1 = float(input('POR FAVOR, USE PONTO (.) COMO SEPARADOR DOS CENTAVOS, CASO PRECISE - R$ '))
@@ -33,13 +39,17 @@ while opção != 3:
 		n2 = float(input('POR FAVOR, USE PONTO (.) COMO SEPARADOR DOS CENTAVOS, CASO PRECISE - R$ '))
 		n2_limitado = round(n2, 2)
 		produto = n1 / n2
+		print(line)
+		sleep(0.5)
+		print(line)
+		sleep(0.5)
 		print('TENDO R${} NA CARTEIRA, COM O DÓLAR VALENDO R${} REAIS, VOCÊ TERIA O EQUIVALENTE A US$ {:.2f} DÓLARES'.format(n1_limitado, n2_limitado, produto))
 	elif opção == 3:
 		print('FINALIZANDO...')
 	else:
 		print('OPÇÃO INVÁLIDA. TENTE NOVAMENTE')
-	print('-=-=-' * 21)
+	print(line)
 	sleep(1)
 print('FIM DO PROGRAMA! VOLTE SEMPRE!')
-print('-=-=-' * 21)
-sleep(3)
+print(line)
+sleep(2)
