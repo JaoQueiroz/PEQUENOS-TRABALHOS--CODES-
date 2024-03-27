88f7ca8d273cefdd87a8c7d85116726678a231ad
João Queiroz <154699209+JaoQueiroz@users.noreply.github.com>
2024-03-27 00:20:09 -0300

    Add files via upload

---
+++ cro/js/scripts.js
@@ -0,0 +1,26 @@
+const minutesEl = document.querySelector("#minutes")
+const secondsEl = document.querySelector("#seconds")
+const millisecondsEl = document.querySelector("#milliseconds")
+const startBtn = document.querySelector("#startBtn")
+const pauseBtn = document.querySelector("#pauseBtn")
+const resumeBtn = document.querySelector("#resumeBtn")
+const resetBtn = document.querySelector("#resetBtn")
+
+let interval;
+let minutes = 0;
+let seconds = 0;
+let milliseconds = 0;
+let isPaused = false;
+
+startBtn.addEventListener("click", starTimer)
+
+function stratTime() {
+
+    interval = setInterval(()
+    
+    
+    
+    )
+
+
+}
